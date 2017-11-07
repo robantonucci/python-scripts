a95244eff387067c95e909603cf1fb7af2942e58
robantonucci <rob.antonucci@me.com>
2017-11-07 13:51:14 -0500

    Changed to OOP

--- decoder.py
+++ decoder.py
@@ -1,69 +1,82 @@
 #!/usr/bin/env python
-
-########################################################################
-#
-# name:     decoder.py
-# created:  4/13/2016
-# updated:  5/13/2016
-# author:   rob antonucci
-# descript: For easy python decoding/encoding
-# to do:    
-#
-########################################################################
-
 import codecs
 import base64
-import sys
+from sys import argv
 import argparse
 import urllib
 import hashlib
 
 parser = argparse.ArgumentParser(
-    description="This script will encode/decode strings")
-parser.add_argument("string", help="Target String")
+  description="This script will encode/decode strings")
+parser.add_argument("input_string", help="Input String")
 
 first_group = parser.add_mutually_exclusive_group()
-first_group.add_argument("-e", "--encode",
-    help="Encode String", action="store_true")
-first_group.add_argument("-d", "--decode",
-    help="Decode String", action="store_true")  
+first_group.add_argument(
+  "-e", "--encode", help="Encode String", action="store_true")
+first_group.add_argument(
+  "-d", "--decode", help="Decode String", action="store_true")
 
-second_group = parser.add_mutually_exclusive_group()      
-second_group.add_argument("--base64",
-    help="Base64", action="store_true")
-second_group.add_argument("--md5",
-    help="MD5", action="store_true")    
-second_group.add_argument("--rot13",
-    help="ROT13", action="store_true")
-second_group.add_argument("--url",
-    help="URL", action="store_true")    
+second_group = parser.add_mutually_exclusive_group()
+second_group.add_argument(
+  "--base64", help="Base64", action="store_true")
+second_group.add_argument(
+  "--md5", help="MD5", action="store_true")
+second_group.add_argument(
+  "--rot13", help="ROT13", action="store_true")
+second_group.add_argument(
+  "--url", help="URL", action="store_true")
 
-if len(sys.argv) == 1:
+if len(argv) == 3:
     parser.print_help()
-    sys.exit(1)
+    exit(1)
 args = parser.parse_args()
 
+
+class Str:
+
+    def __init__(self, input_string, e):
+        self.input_string = input_string
+        self.e = e
+
+    def base64(self):
+        if self.e:
+            return base64.b64encode(self.input_string)
+        else:
+            return base64.b64decode(self.input_string)
+
+    def rot13(self):
+        if self.e:
+            return codecs.encode(self.input_string, 'rot13')
+        else:
+            return codecs.decode(self.input_string, 'rot13')
+
+    def url(self):
+        if self.e:
+            return urllib.quote_plus(self.input_string)
+        else:
+            return urllib.unquote(self.input_string)
+
+    def md5(self):
+        if self.e:
+            h = hashlib.md5()
+            h.update(self.input_string)
+            return h.hexdigest()
+        else:
+            return "Need to crack MD5 :)"
+
+
+if args.encode:
+    str_object = Str(args.input_string, 1)
+else:
+    str_object = Str(args.input_string, 0)
+
 if args.base64:
-    if args.encode:
-        print base64.b64encode(args.string)
-    elif args.decode:
-        print base64.b64decode(args.string)
+    print str_object.base64()
 elif args.rot13:
-    if args.encode:
-        print codecs.encode(args.string,'rot13')
-    elif args.decode:
-        print codecs.decode(args.string,'rot13')        
+    print str_object.rot13()
 elif args.url:
-    if args.encode:
-        print urllib.quote_plus(args.string)
-    elif args.decode:
-        print urllib.unquote(args.string)
+    print str_object.url()
 elif args.md5:
-    if args.encode:
-        m = hashlib.md5()
-        m.update(args.string)
-        print m.hexdigest()
-    elif args.decode:
-        print "Need to crack MD5 :)"
-
-
+    print str_object.md5()
+else:
+    print "Please specify a valid encoder/decoder!"
